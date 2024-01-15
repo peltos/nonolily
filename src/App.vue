98b59e4e38b643f4a91acd20f95867e542ca2085
Ron Pelt <ron.pelt@blueelevation.nl>
2024-01-15 15:24:53 +0100

    Added icons for the left and right mouse buttons

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,65 @@
 <template>
-
-  <NonoNav @changeBoardSize="changeBoardSize"/>
+  <NonoNav @changeBoardSize="changeBoardSize" />
 
   <!-- Main content -->
   <div class="flex justify-center items-center h-full flex-col transition-all">
 
     <!-- Lives -->
-    <NonoLives :lives="lives"/>
+    <NonoLives :lives="lives" />
 
     <div v-if="lives > 0" class="nonogram transition-all drop-shadow-lg">
       <div class="nonogram-top w-full flex transition-all">
         <div v-for="(row, rowIndex) in boardlayoutColumn" v-bind:key="row + '-' + rowIndex"
           class="nono-top-cell flex flex-col justify-end items-center pt-2 pb-2 border-solid border border-gray-200 transition-all">
           <!-- Hint Top columns -->
-          <NonoHint v-for="(hint, indexHint) in calculateHints(row)" v-bind:key="indexHint + '-' + hint" :hint="hint"/>
+          <NonoHint v-for="(hint, indexHint) in calculateHints(row)" v-bind:key="indexHint + '-' + hint" :hint="hint" />
         </div>
       </div>
       <div class="nonogram-left h-full transition-all">
         <div v-for="(row, rowIndex) in boardlayoutRow" v-bind:key="row + '-' + rowIndex"
           class="nono-left-cell flex justify-end items-center pr-2 pl-2 border-solid border border-gray-200 transition-all">
           <!-- Hint Left row -->
-          <NonoHint v-for="(hint, indexHint) in calculateHints(row)" v-bind:key="indexHint + '-' + hint" :hint="hint"/>
+          <NonoHint v-for="(hint, indexHint) in calculateHints(row)" v-bind:key="indexHint + '-' + hint" :hint="hint" />
         </div>
       </div>
       <div class="nonogram-board transition-all">
         <div v-for="(row, indexRow) in boardlayoutRow" v-bind:key="row" class="nono-row flex">
           <template v-for="(cellValue, indexColumn) in row" v-bind:key="cellValue">
             <!-- Cell -->
-            <NonoCell @clickedFilledCell="reduceCellCount(indexRow, indexColumn)" @mistakeMade="lives = lives - 1" @greyOut="greyOut = 'grey'"
-              :cellValue="cellValue" :cellRow="indexRow" :cellColumn="indexColumn" :full="full"></NonoCell>
+            <NonoCell @clickedFilledCell="reduceCellCount(indexRow, indexColumn)" @mistakeMade="lives = lives - 1"
+              @greyOut="greyOut = 'grey'" :cellValue="cellValue" :cellRow="indexRow" :cellColumn="indexColumn"
+              :full="full"></NonoCell>
           </template>
         </div>
       </div>
     </div>
-    <strong v-if="lives > 0" class="nono-tip">Left click = fill<br> Right click = cross </strong>
+    <div v-if="lives > 0" class="nono-tip flex">
+
+      <div>
+        <div class="example-cell fill"></div>
+
+        <svg fill="#000000" version="1.1" viewBox="0 0 356.57 356.57" xml:space="preserve"
+          xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="m181.56 0c-60.801 0-122.35 30.525-122.35 88.873v148.63c0 65.658 53.412 119.07 119.07 119.07 65.658 0 119.07-53.413 119.07-119.07v-148.63c0-61.064-60.02-88.873-115.79-88.873zm93.382 237.5c0 53.303-43.362 96.657-96.659 96.657-53.299 0-96.657-43.354-96.657-96.657v-69.513c20.014 6.055 57.685 15.215 102.22 15.215 28.515 0 59.831-3.809 91.095-14.567v68.865zm0-92.706c-81.683 31.233-168.35 7.716-193.32-0.364v-55.557c0-43.168 51.489-66.46 99.934-66.46 46.481 0 93.382 20.547 93.382 66.46v55.921zm-84.052-96.405v81.248c0 6.187-5.023 11.208-11.206 11.208-6.185 0-11.207-5.021-11.207-11.208v-81.248c0-6.186 5.021-11.207 11.207-11.207 6.182 0 11.206 5.021 11.206 11.207zm-35.955-8.321v103.66c-15.879 2.802-62.566-10.271-62.566-10.271-12.139-92.455 62.566-93.391 62.566-93.391z" />
+        </svg>
+      </div>
+      <div>
+        <div class="example-cell cross">
+          <svg xmlns="http://www.w3.org/2000/svg" height="16" width="12" viewBox="0 0 384 512">
+            <path opacity="1"
+              d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+          </svg>
+        </div>
+
+        <svg fill="#000000" version="1.1" viewBox="0 0 356.57 356.57" xml:space="preserve"
+          xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="m181.56 0c-60.801 0-122.35 30.525-122.35 88.873v148.63c0 65.658 53.412 119.07 119.07 119.07 65.658 0 119.07-53.413 119.07-119.07v-148.63c0-61.064-60.02-88.873-115.79-88.873zm93.382 237.5c0 53.303-43.362 96.657-96.659 96.657-53.299 0-96.657-43.354-96.657-96.657v-69.513c20.014 6.055 57.685 15.215 102.22 15.215 28.515 0 59.831-3.809 91.095-14.567v68.865zm0-92.706c-81.683 31.233-168.35 7.716-193.32-0.364v-55.557c0-43.168 51.489-66.46 99.934-66.46 46.481 0 93.382 20.547 93.382 66.46v55.921zm-84.052-96.405v81.248c0 6.187-5.023 11.208-11.206 11.208-6.185 0-11.207-5.021-11.207-11.208v-81.248c0-6.186 5.021-11.207 11.207-11.207 6.182 0 11.206 5.021 11.206 11.207zm73.379 81.989s-46.687 13.072-62.566 10.271v-103.66s74.706 0.935 62.566 93.39z" />
+        </svg>
+      </div>
+
+    </div>
     <h2 class="nono-error" v-if="lives <= 0">Game over...</h2>
   </div>
 
@@ -102,7 +128,7 @@ function boardInit() {
   // TODO: remove, is for debugging
   console.table(boardlayoutRow.value.map(row => {
     return row.map(cell => {
-      return cell ? cell = '✅': cell = '❌';
+      return cell ? cell = '✅' : cell = '❌';
     });
   }));
 }
@@ -113,7 +139,7 @@ function changeBoardSize(settings) {
   lives.value = settings.lives;
   sizeBoardColums.value = settings.sizeBoardColums;
   sizeBoardRows.value = settings.sizeBoardRows;
-  
+
   boardInit();
 }
 
@@ -175,28 +201,28 @@ h2 {
   grid-template-rows: auto auto;
 
   .nonogram-top {
-  grid-row: 1 / 2;
-  grid-column: 2 / 3;
-  background: white;
-  @apply border;
-  @apply border-gray-500;
+    grid-row: 1 / 2;
+    grid-column: 2 / 3;
+    background: white;
+    @apply border;
+    @apply border-gray-500;
 
-  .nono-top-cell {
-    width: calc(var(--size-cell) * 1px);
+    .nono-top-cell {
+      width: calc(var(--size-cell) * 1px);
+    }
   }
-}
 
-.nonogram-left {
-  grid-row: 2 / 3;
-  grid-column: 1 / 2;
-  background: white;
-  @apply border;
-  @apply border-gray-500;
+  .nonogram-left {
+    grid-row: 2 / 3;
+    grid-column: 1 / 2;
+    background: white;
+    @apply border;
+    @apply border-gray-500;
 
-  .nono-left-cell {
-    height: calc(var(--size-cell) * 1px);
+    .nono-left-cell {
+      height: calc(var(--size-cell) * 1px);
+    }
   }
-}
 
   .nonogram-board {
     grid-row: 2 / 3;
@@ -225,7 +251,34 @@ h2 {
   }
 }
 
-.nono-tip,
+.nono-tip {
+  gap: 16px;
+  margin: 32px 8px 8px;
+
+  .example-cell {
+    background-color: white;
+    transition: 200ms;
+    width: calc(var(--size-cell) * 1px);
+    height: calc(var(--size-cell) * 1px);
+    margin-bottom: 8px;
+
+    svg {
+      width: calc(var(--size-cell) * 1px);
+      height: calc(var(--size-cell) * 1px);
+    }
+
+    &.fill {
+      background-color: black;
+    }
+
+    &.cross {
+      svg {
+        @apply fill-gray-400;
+      }
+    }
+  }
+}
+
 .nono-error {
   font-size: calc((var(--size-cell) * 1px) / 2);
   line-height: calc((var(--size-cell) * 1px) / 1.5);
